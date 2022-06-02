c8bd5fddd9721eddcdf01c104f6a14471dccc3af
medriel <adriel.souza397@gmail.com>
2022-06-02 20:38:18 -0300

    Create user use case

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,28 @@
+import { User } from "@prisma/client";
+import { prisma } from "../../../../prisma/client";
+import { CreateUserDTO } from "../../dtos/CreateUserDTO";
+
+export class CreateUserUseCase {
+  async execute({ name, email }: CreateUserDTO): Promise<User> {
+    // Verificar se o usuário já existe
+    const userAlreadyExists = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
+
+    if (userAlreadyExists) {
+      //error
+    }
+
+    // Criar usuário
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email
+      }
+    });
+
+    return user;
+  }
+}
