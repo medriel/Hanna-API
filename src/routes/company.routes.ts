6451cf5b11d3782b346e0ded8472623a1c01b5e4
medriel <adriel.souza397@gmail.com>
2022-06-03 20:59:16 -0300

    Insert get in companies router

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateCompanyController } from "../modules/companies/UseCases/createCompany/CreateCompanyController";
+import { ListCompaniesController } from "../modules/companies/UseCases/listCompanies/ListCompaniesController";
 
 const companiesRoutes = Router();
 
 const createCompanyController = new CreateCompanyController();
+const listCompaniesController = new ListCompaniesController();
 
 companiesRoutes.post("/", createCompanyController.handle);
+companiesRoutes.get("/", listCompaniesController.handle)
 
 export { companiesRoutes }
