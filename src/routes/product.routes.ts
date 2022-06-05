7d5efb503b251e7eb3975232fc47018beafcb15d
medriel <adriel.souza397@gmail.com>
2022-06-05 11:19:10 -0300

    Create router product location

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { CreateProductController } from "../modules/products/UseCases/createProduct/CreateProductController";
+import { CreateProductLocationController } from "../modules/products/UseCases/createProductLocation/CreateProductLocationController";
 import { ListProductsController } from "../modules/products/UseCases/listProducts/ListProductsController";
 
 const productRoutes = Router();
 
 const createProductController = new CreateProductController();
 const listProductsController = new ListProductsController();
+const createProductLocationController = new CreateProductLocationController();
 
 productRoutes.post("/", createProductController.handle);
 
 productRoutes.get("/", listProductsController.handle);
+
+productRoutes.post("/location", createProductLocationController.handle);
 
 export { productRoutes }
