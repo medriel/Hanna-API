bb89b28d55e53bc1a64153adeba977ab554d37dc
medriel <adriel.souza397@gmail.com>
2022-06-05 11:34:07 -0300

    Update list Locations

--- src/modules/locations/UseCases/listLocations/ListLocationsUseCase.ts
+++ src/modules/locations/UseCases/listLocations/ListLocationsUseCase.ts
@@ -3,7 +3,19 @@ import { prisma } from "../../../../prisma/client";
 
 export class ListLocationsUseCase {
   async execute(): Promise<Location[]> {
-    const locations = await prisma.location.findMany({});
+    const locations = await prisma.location.findMany({
+      include: {
+        ProductLocation: {
+          select: {
+            product: {
+              select: {
+                name: true,
+              }
+            }
+          }
+        }
+      }
+    });
 
     return locations;
   }
