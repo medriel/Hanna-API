0db51c16b7bee293fcf1d791f7392a7c238e32eb
medriel <adriel.souza397@gmail.com>
2022-06-05 15:56:47 -0300

    Create router update product

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -2,17 +2,21 @@ import { Router } from "express";
 import { CreateProductController } from "../modules/products/UseCases/createProduct/CreateProductController";
 import { CreateProductLocationController } from "../modules/products/UseCases/createProductLocation/CreateProductLocationController";
 import { ListProductsController } from "../modules/products/UseCases/listProducts/ListProductsController";
+import { UpdateProductController } from "../modules/products/UseCases/updateProduct/UpdateProductController";
 
 const productRoutes = Router();
 
 const createProductController = new CreateProductController();
 const listProductsController = new ListProductsController();
 const createProductLocationController = new CreateProductLocationController();
+const updateProductController = new UpdateProductController();
 
 productRoutes.post("/", createProductController.handle);
 
 productRoutes.get("/", listProductsController.handle);
 
 productRoutes.post("/location", createProductLocationController.handle);
+
+productRoutes.patch("/:id", updateProductController.handle);
 
 export { productRoutes }
