886489242e3cc5f8d368d80f8a817370902932dc
medriel <adriel.souza397@gmail.com>
2022-06-05 11:34:54 -0300

    Update list Users

--- src/modules/users/useCases/listUsers/ListUsersUseCase.ts
+++ src/modules/users/useCases/listUsers/ListUsersUseCase.ts
@@ -4,7 +4,15 @@ import { prisma } from "../../../../prisma/client";
 export class ListUsersUseCase {
   async execute(): Promise<User[]> {
 
-    const users = await prisma.user.findMany({});
+    const users = await prisma.user.findMany({
+      include: {
+        company: {
+          select: {
+            name: true
+          }
+        }
+      }
+    });
 
     return users;
   }
