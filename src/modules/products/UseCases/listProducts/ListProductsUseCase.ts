642274b415ad3c8c96636e625ce23a5f300f3d54
medriel <adriel.souza397@gmail.com>
2022-06-05 11:34:34 -0300

    Update list products

--- src/modules/products/UseCases/listProducts/ListProductsUseCase.ts
+++ src/modules/products/UseCases/listProducts/ListProductsUseCase.ts
@@ -4,7 +4,15 @@ import { prisma } from "../../../../prisma/client";
 export class ListProductsUseCase {
   async execute(): Promise<Product[]> {
 
-    const products = await prisma.product.findMany({});
+    const products = await prisma.product.findMany({
+      include: {
+        ProductLocation: {
+          select: {
+            location: true
+          }
+        }
+      }
+    });
 
     return products;
   }
