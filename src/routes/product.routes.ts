eecf02d58e4de342d04357d92391b6730ee0a619
medriel <adriel.souza397@gmail.com>
2022-06-06 09:24:54 -0300

    Create router delete product

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateProductController } from "../modules/products/UseCases/createProduct/CreateProductController";
 import { CreateProductLocationController } from "../modules/products/UseCases/createProductLocation/CreateProductLocationController";
+import { DeleteProductController } from "../modules/products/UseCases/deleteProduct/DeleteProductController";
 import { ListProductsController } from "../modules/products/UseCases/listProducts/ListProductsController";
 import { UpdateProductController } from "../modules/products/UseCases/updateProduct/UpdateProductController";
 
@@ -10,6 +11,7 @@ const createProductController = new CreateProductController();
 const listProductsController = new ListProductsController();
 const createProductLocationController = new CreateProductLocationController();
 const updateProductController = new UpdateProductController();
+const deleteProductController = new DeleteProductController();
 
 productRoutes.post("/", createProductController.handle);
 
@@ -18,5 +20,7 @@ productRoutes.get("/", listProductsController.handle);
 productRoutes.post("/location", createProductLocationController.handle);
 
 productRoutes.patch("/:id", updateProductController.handle);
+
+productRoutes.delete("/:id", deleteProductController.handle);
 
 export { productRoutes }
