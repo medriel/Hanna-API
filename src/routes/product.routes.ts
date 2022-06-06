9fd4ce437b0832870308ee4d93e6600808207b37
medriel <adriel.souza397@gmail.com>
2022-06-06 10:11:17 -0300

    Create router update product location

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -4,6 +4,7 @@ import { CreateProductLocationController } from "../modules/products/UseCases/cr
 import { DeleteProductController } from "../modules/products/UseCases/deleteProduct/DeleteProductController";
 import { ListProductsController } from "../modules/products/UseCases/listProducts/ListProductsController";
 import { UpdateProductController } from "../modules/products/UseCases/updateProduct/UpdateProductController";
+import { UpdateProductLocationController } from "../modules/products/UseCases/updateProductLocation/UpdateProductLocationController";
 
 const productRoutes = Router();
 
@@ -11,6 +12,7 @@ const createProductController = new CreateProductController();
 const listProductsController = new ListProductsController();
 const createProductLocationController = new CreateProductLocationController();
 const updateProductController = new UpdateProductController();
+const updateProductLocationController = new UpdateProductLocationController();
 const deleteProductController = new DeleteProductController();
 
 productRoutes.post("/", createProductController.handle);
@@ -20,6 +22,8 @@ productRoutes.get("/", listProductsController.handle);
 productRoutes.post("/location", createProductLocationController.handle);
 
 productRoutes.patch("/:id", updateProductController.handle);
+
+productRoutes.put("/:product_id/:location_id", updateProductLocationController.handle);
 
 productRoutes.delete("/:id", deleteProductController.handle);
 
