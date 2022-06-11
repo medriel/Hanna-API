2ed3ec682f939df422aecb5f80e9e38f976c395e
medriel <adriel.souza397@gmail.com>
2022-06-11 07:58:54 -0300

    Update location routes

--- src/routes/location.routes.ts
+++ src/routes/location.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { CreateLocationController } from "../modules/locations/UseCases/createLocation/CreateLocationController";
+import { DeleteLocationController } from "../modules/locations/UseCases/deleteLocation/DeleteLocationController";
 import { ListLocationsController } from "../modules/locations/UseCases/listLocations/ListLocationsController";
 
 const locationRouter = Router();
 
 const createLocationController = new CreateLocationController();
 const listLocationsController = new ListLocationsController();
+const deleteLocationController = new DeleteLocationController();
 
 locationRouter.post("/", createLocationController.handle);
 
 locationRouter.get("/", listLocationsController.handle);
+
+locationRouter.delete("/:id", deleteLocationController.handle);
 
 export { locationRouter }
