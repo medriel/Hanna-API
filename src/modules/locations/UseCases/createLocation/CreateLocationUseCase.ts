2e4439bb3df6e4267d865ddb9c955eca33ee0ee5
medriel <adriel.souza397@gmail.com>
2022-07-11 20:30:35 -0300

    Prevent repeated location registration

--- src/modules/locations/UseCases/createLocation/CreateLocationUseCase.ts
+++ src/modules/locations/UseCases/createLocation/CreateLocationUseCase.ts
@@ -1,9 +1,22 @@
 import { Location } from "@prisma/client";
+import { AppError } from "../../../../errors/AppError";
 import { prisma } from "../../../../prisma/client";
 import { CreateLocationDTO } from "../../dtos/CreateLocationDTO";
 
 export class CreateLocationUseCase {
   async execute({ localization, type, reference }: CreateLocationDTO): Promise<Location> {
+
+    // Verificar se a localização já existe
+    const locationAlreadyExists = await prisma.location.findFirst({
+      where: {
+        localization,
+        type,
+        reference
+      }
+    });
+    if (locationAlreadyExists) {
+      throw new AppError("Location already exists!");
+    }
 
     const product = await prisma.location.create({
       data: { localization, type, reference }
