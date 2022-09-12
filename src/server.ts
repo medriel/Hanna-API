01cd48c680814ff32f041f5618df1207efd27eaf
medriel <adriel.souza397@gmail.com>
2022-09-12 20:27:36 -0300

    Add port env variable

--- src/server.ts
+++ src/server.ts
@@ -31,4 +31,4 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
   })
 })
 
-app.listen(3333, () => console.log("Server is running"));
+app.listen(process.env.PORT || 3333, () => console.log("Server is running"));
